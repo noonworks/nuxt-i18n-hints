7c58ebe28e657acfeacdf98bed409f882c8a9165
noonworks <noonworks@gmail.com>
2020-01-12 02:57:21 +0900

    Add hooks.

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,46 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const options_1 = require("./options");
+const chokidar = require("chokidar");
+const vue_i18n_hints_1 = require("vue-i18n-hints");
 const NuxtI18nHintsModule = function (moduleOptions) {
+    // get option
     const opt = options_1.mergeOption(this.options, moduleOptions);
-    console.log(opt);
-    this.nuxt.hook('build:compile', 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (params) => {
-        console.log(params.name);
-        console.log(typeof params.compiler);
+    const hintCompiler = new vue_i18n_hints_1.HintCompiler({
+        sourceDir: opt.hint.sourceDir,
+        outDir: opt.hint.outDir,
+        postfix: opt.hint.postfix
+    });
+    const miniTranspiler = new vue_i18n_hints_1.MiniTranspiler({
+        sourceDir: opt.messages.sourceDir,
+        outDir: opt.messages.outDir
+    });
+    // chokidar
+    const chokidars = {};
+    // set hook
+    this.nuxt.hook('build:compile', (params) => {
+        if (params.name === 'server')
+            return;
+        if (!params.compiler)
+            return;
+        // watch hints file
+        if (opt.hint.source.length > 0) {
+            chokidars.hint = chokidar
+                .watch(opt.hint.source)
+                .on('change', (path) => {
+                console.log('change ' + path);
+                hintCompiler.compile([path]);
+            });
+        }
+        // watch ts -> js files
+        if (opt.messages.sources.length > 0) {
+            chokidars.js = chokidar
+                .watch(opt.messages.sources)
+                .on('change', (path) => {
+                console.log('change ' + path);
+                miniTranspiler.compile([path]);
+            });
+        }
     });
 };
 exports.default = NuxtI18nHintsModule;
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,55 @@
 import { Module } from '@nuxt/types';
 import { Options, mergeOption, NuxtModuleThis } from './options';
+import * as webpack from 'webpack';
+import * as chokidar from 'chokidar';
+import { HintCompiler, MiniTranspiler } from 'vue-i18n-hints';
+
+interface ChokidarDict {
+  hint?: chokidar.FSWatcher;
+  js?: chokidar.FSWatcher;
+}
 
 const NuxtI18nHintsModule: Module<Options> = function(
   this: NuxtModuleThis,
   moduleOptions?: Partial<Options>
 ): void {
+  // get option
   const opt = mergeOption(this.options, moduleOptions);
-  console.log(opt);
+  const hintCompiler = new HintCompiler({
+    sourceDir: opt.hint.sourceDir,
+    outDir: opt.hint.outDir,
+    postfix: opt.hint.postfix
+  });
+  const miniTranspiler = new MiniTranspiler({
+    sourceDir: opt.messages.sourceDir,
+    outDir: opt.messages.outDir
+  });
+  // chokidar
+  const chokidars: ChokidarDict = {};
+  // set hook
   this.nuxt.hook(
     'build:compile',
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (params: { name: 'client' | 'server'; compiler: any }) => {
-      console.log(params.name);
-      console.log(typeof params.compiler);
+    (params: { name: 'client' | 'server'; compiler: webpack.Compiler }) => {
+      if (params.name === 'server') return;
+      if (!params.compiler) return;
+      // watch hints file
+      if (opt.hint.source.length > 0) {
+        chokidars.hint = chokidar
+          .watch(opt.hint.source)
+          .on('change', (path: string) => {
+            console.log('change ' + path);
+            hintCompiler.compile([path]);
+          });
+      }
+      // watch ts -> js files
+      if (opt.messages.sources.length > 0) {
+        chokidars.js = chokidar
+          .watch(opt.messages.sources)
+          .on('change', (path: string) => {
+            console.log('change ' + path);
+            miniTranspiler.compile([path]);
+          });
+      }
     }
   );
 };
